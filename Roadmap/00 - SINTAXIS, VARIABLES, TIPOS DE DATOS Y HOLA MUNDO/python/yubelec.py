87861c436374683b3ce265191ece4db76b30b6d2
segemind <sistemas@segemind.com>
2024-04-19 20:58:19 -0500

    #00 - python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/yubelec.py
@@ -0,0 +1,26 @@
+# https://www.python.org/
+
+"""
+Comentario en varias líneas
+Comentario en varias líneas
+Comentario en varias líneas
+"""
+
+''' 
+Comentario en varias líneas
+Comentario en varias líneas
+Comentario en varias líneas
+'''
+
+mi_variable = 'mi variable'
+
+# No hay constantes en python
+
+tipo_texto= "Yubel"
+tipo_entero = 2
+tipo_float = 3.656
+tipo_boolean= True
+
+print('Mi nombre es: '+tipo_texto)
+print("¡Hola, [Python]!")
+
